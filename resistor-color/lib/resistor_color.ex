2678e72d93575831c0283868681994221b3301e8
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2020-04-01 01:43:51 +0600

    solved resistor-color

--- resistor-color/lib/resistor_color.ex
+++ resistor-color/lib/resistor_color.ex
@@ -1,11 +1,24 @@
 defmodule ResistorColor do
   @moduledoc false
-
+  @colors [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white"
+  ]
   @spec colors() :: list(String.t())
   def colors do
+    @colors
   end
 
   @spec code(String.t()) :: integer()
   def code(color) do
+    Enum.find_index(@colors, &(&1 == color))
   end
 end
