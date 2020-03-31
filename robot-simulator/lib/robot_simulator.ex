fb4ccb1f311a0514913fe470b31fd9649fc64b9d
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2020-04-01 01:01:16 +0600

    solved robot-simulator

--- robot-simulator/lib/robot_simulator.ex
+++ robot-simulator/lib/robot_simulator.ex
@@ -1,11 +1,26 @@
 defmodule RobotSimulator do
+  @valid_directions [:north, :east, :south, :west]
   @doc """
   Create a Robot Simulator given an initial direction and position.
 
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
   @spec create(direction :: atom, position :: {integer, integer}) :: any
-  def create(direction \\ nil, position \\ nil) do
+  def create(direction \\ :north, position \\ {0, 0}) do
+    cond do
+      Enum.member?(@valid_directions, direction) == false ->  {:error, "invalid direction"}
+      not_valid?(position) -> {:error, "invalid position"}
+      true -> %{
+        :direction => direction,
+        :position => position,
+      }
+    end 
+  end
+  def not_valid?({x, y}) do
+    (is_integer(x) and is_integer(y) == true) == false
+  end
+  def not_valid?(_) do
+    true
   end
 
   @doc """
@@ -14,7 +29,40 @@ defmodule RobotSimulator do
   Valid instructions are: "R" (turn right), "L", (turn left), and "A" (advance)
   """
   @spec simulate(robot :: any, instructions :: String.t()) :: any
-  def simulate(robot, instructions) do
+  def simulate(robot, "L"<>instructions) do
+      case robot.direction do
+        :east  -> Map.update!(robot, :direction, fn _ -> :north end) |> simulate(instructions)
+        :west  -> Map.update!(robot, :direction, fn _ -> :south end) |> simulate(instructions)
+        :north -> Map.update!(robot, :direction, fn _ -> :west end) |> simulate(instructions)
+        :south -> Map.update!(robot, :direction, fn _ -> :east end) |> simulate(instructions)
+      end
+
+  end
+
+  def simulate(robot, "R"<>instructions) do
+      case robot.direction do
+        :east  -> Map.update!(robot, :direction, fn _ -> :south end) |> simulate(instructions)
+        :west  -> Map.update!(robot, :direction, fn _ -> :north end) |> simulate(instructions)
+        :north -> Map.update!(robot, :direction, fn _ -> :east end) |> simulate(instructions)
+        :south -> Map.update!(robot, :direction, fn _ -> :west end) |> simulate(instructions)
+      end
+  end
+
+  def simulate(robot, "A"<>instructions) do
+      case robot.direction do
+        :east  -> Map.update!(robot, :position, fn {x,y} -> {x+1, y} end) |> simulate(instructions)
+        :west  -> Map.update!(robot, :position, fn {x,y} -> {x-1, y} end) |> simulate(instructions)
+        :north -> Map.update!(robot, :position, fn {x,y} -> {x, y+1} end) |> simulate(instructions)
+        :south -> Map.update!(robot, :position, fn {x,y} -> {x, y-1} end) |> simulate(instructions)
+      end
+  end
+
+  def simulate(robot, "") do
+    robot
+  end
+
+  def simulate(robot, _) do
+    {:error, "invalid instruction"}
   end
 
   @doc """
@@ -24,6 +72,7 @@ defmodule RobotSimulator do
   """
   @spec direction(robot :: any) :: atom
   def direction(robot) do
+    robot.direction
   end
 
   @doc """
@@ -31,5 +80,6 @@ defmodule RobotSimulator do
   """
   @spec position(robot :: any) :: {integer, integer}
   def position(robot) do
+    robot.position 
   end
 end
