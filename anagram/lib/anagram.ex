3647331880f495535db0d16ecf1806cc66aa0f59
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2020-04-22 05:57:13 +0600

    solved anagram

--- anagram/lib/anagram.ex
+++ anagram/lib/anagram.ex
@@ -4,5 +4,16 @@ defmodule Anagram do
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
+    candidates 
+    |> Enum.filter(&anagram?(String.downcase(base), String.downcase(&1)))
   end
+
+  def anagram?(word, word), do: false
+  def anagram?(word, candidate), do: count_letters(word) == count_letters(candidate)
+
+  def count_letters(word) do
+    word = word |> String.graphemes() 
+    Enum.map(word, &({&1, Enum.count(word, fn c -> c == &1 end)})) 
+    |> Map.new()
+  end 
 end
